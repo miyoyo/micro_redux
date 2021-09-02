e67b17ee513f8c2292cb49890091514592cc3cf7
miyoyo <miyoyo@live.be>
2021-09-02 14:30:54 +0200

    Provides copy of the state, instead of the state itself
    Additionally, remove the action chain stream.

--- micro_redux.dart
+++ micro_redux.dart
@@ -1,50 +1,49 @@
 import 'dart:async';
 
-typedef Reducer<State> = Future<State> Function(
+typedef Reducer<State extends StoreState> = Future<State> Function(
     State currentState, Action action);
 
-typedef Middleware<State> = Future<void> Function(
+typedef Middleware<State extends StoreState> = Future<void> Function(
     Store<State> currentState, Action action, NextDispatcher next);
 
 typedef NextDispatcher = Future<void> Function(Action action);
 
 abstract class Action {}
 
-class Store<State> {
+abstract class StoreState {
+  StoreState copyWith();
+}
+
+class Store<State extends StoreState> {
   Store(Reducer<State> reducer,
       {required State initialState,
       List<Middleware<State>> middleware = const [],
       bool syncStream = false,
       bool distinct = false})
       : _state = initialState,
-        _controller = StreamController<State>.broadcast(sync: syncStream),
-        _actionStream = StreamController<Action>() {
+        _controller = StreamController<State>.broadcast(sync: syncStream) {
     _chain = [
       for (var i = 0; i < middleware.length; i++)
         (Action action) => middleware[i](this, action, _chain[i + 1]),
       (Action action) async {
         final newState = await reducer(_state, action);
-        if (!distinct || newState != _state) _controller.add(_state = newState);
+        if (!distinct || newState != _state) {
+          _state = newState;
+          _controller.add(state);
+        }
       }
     ];
-    _actionStream.stream.listen(_chain.first);
   }
 
   State _state;
-  State get state => _state;
+  State get state => _state.copyWith() as State;
 
   late List<NextDispatcher> _chain;
 
   StreamController<State> _controller;
   Stream<State> get onChange => _controller.stream;
 
-  StreamController<Action> _actionStream;
-  Sink<Action> get dispatchStream => _actionStream.sink;
+  void dispatch(Action action) => _chain.first(action);
 
-  void dispatch(Action action) => dispatchStream.add(action);
-
-  Future<void> dispose() async {
-    _actionStream.close();
-    _controller.close();
-  }
+  Future<void> dispose() => _controller.close();
 }
